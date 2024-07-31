b906dbd6944e38186c44fe2f5b3398ce31578c35
Sridharchetty <sridharan425@gmail.com>
2024-07-31 07:10:07 +0530

    React Basic

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("div", { className: "parent" }, [
-  React.createElement("div", { className: "child" }, [
-    React.createElement("h1", {}, "I'm a H1 tag"),
-    React.createElement("p", {}, "I'm a P tag"),
-  ]),
-  React.createElement("div", { className: "child" }, [
-    React.createElement("h1", {}, "I'm a H1 tag"),
-    React.createElement("p", {}, "I'm a P tag"),
-  ]),
-]);
-console.log(parent);
-root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * - Header Component
+ *   - Logo Component
+ *   - Navbar Component
+ * - Main Body Component
+ *   - Main Restobar Container
+ *     - Restaurant Card Container
+ * - Footer Component
+ *   - Copyrights
+ *   - Links
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // Root element mapping
+const ForNestComponent = function () {
+  return (
+    <h3 className="component3">
+      This is h3 component <sub>sub of the h3 component</sub>
+    </h3>
+  );
+};
+const HeadingComponent = () => (
+  <div className="container">
+    <h1 className="component">This is h1 functional component!</h1>
+    <h2 className="component2">This is h2 functional component</h2>
+    <ForNestComponent />
+  </div>
+);
+
+root.render(<HeadingComponent />);
